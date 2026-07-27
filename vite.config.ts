44db5959a805a3564ab863b71ca411ef73ba57ab
Mikel Aldalur <mikelalda13@gmail.com>
2026-07-27 16:05:38 +0200

    Preparando dominio

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig} from 'vite';
 export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
-    base: '/kriston_burrundaie/',
+    base: '/',
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
